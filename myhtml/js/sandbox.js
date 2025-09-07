a4c9ef2748f01ed0e7935f7811fb3829697212db
糞谷ふんすけ <funsuke0327@yahoo.co.jp>
2025-09-07 13:23:28 +0900

    Update sandbox.js

--- myhtml/js/sandbox.js
+++ myhtml/js/sandbox.js
@@ -99,8 +99,8 @@ window.addEventListener("load", function() {
 		function resize(game) {
 			if (!pf.containerController) return;
 			var viewportSize = {
-				width: (window.innerWidth || document.documentElement.clientWidth) * 0.95,
-				height: (window.innerHeight || document.documentElement.clientHeight) * 0.95
+				width: (window.innerWidth || document.documentElement.clientWidth) * 0.97,
+				height: (window.innerHeight || document.documentElement.clientHeight) * 0.97
 			};
 			var gameScale = Math.min(
 				viewportSize.width / game.width,
